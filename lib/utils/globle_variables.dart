211d4296e8d7b83c1ca5ff9c1cb62f7ac7b2e88d
unknown <gauravjoshi747@gmail.com>
2024-06-07 12:03:14 +0530

    app is completed

--- lib/utils/globle_variables.dart
+++ lib/utils/globle_variables.dart
@@ -1,18 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/screen/add_post_screen.dart';
 import 'package:instagram_flutter/screen/feed_screen.dart';
+import 'package:instagram_flutter/screen/profile_screen.dart';
 import 'package:instagram_flutter/screen/search_screen.dart';
 
 const webScreenSize = 600;
 
-const homeScreenItems = [
+List<Widget> homeScreenItems = [
   FeedScreen(),
   SearchScreen(),
   AddPostScreen(),
   Center(
-    child: Text("Four"),
+    child: Text("notifications"),
   ),
-  Center(
-    child: Text("Five"),
+  ProfileScreen(
+    uid: FirebaseAuth.instance.currentUser!.uid,
   ),
 ];
